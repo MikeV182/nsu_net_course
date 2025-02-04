3c789a7c2428e06a6a68bb52ba7d796c29e2d406
MikeV182 <michailvays@gmail.com>
2025-02-04 21:46:23 +0700

    done some refactoring

--- task1/task1.py
+++ task1/task1.py
@@ -5,8 +5,8 @@ try:
 
 
     hostnames = [
-        "google.com", 
-        "stackoverflow.com", 
+        "google.com",
+        "stackoverflow.com",
         "github.com",
         "yahoo.com",
         "yandex.ru",
@@ -14,23 +14,24 @@ try:
         "wikipedia.org",
         "play2048.co",
         "pointerpointer.com",
-        "discord.com"
+        "discord.com",
     ]
 
-    with open('responses.csv', 'w', encoding='UTF8') as f:
-            writer = csv.writer(f)
-            writer.writerow(['web_adress', 'rtt_min (ms)', 'rtt_avg (ms)', 'rtt_max (ms)'])
-            for hostname in hostnames:
-                command = f"ping -c 3 {hostname}"
-                print(f"Executing now: {command}")
+    with open("responses.csv", "w", encoding="UTF8") as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            "web_adress", "rtt_min (ms)", "rtt_avg (ms)", "rtt_max (ms)"])
+        for hostname in hostnames:
+            command = f"ping -c 3 {hostname}"
+            print(f"Executing now: {command}")
 
-                output = subprocess.check_output(command, shell=True, text=True)
-                rtt_substring = re.search('rtt min/avg/max/mdev = ', output)
-                output = output[rtt_substring.start()+23:-3]
-                timings = output.split('/')
+            output = subprocess.check_output(command, shell=True, text=True)
+            rtt_substring = re.search("rtt min/avg/max/mdev = ", output)
+            output = output[rtt_substring.start() + 23 : -3]
+            timings = output.split("/")
 
-                writer.writerow([hostname, timings[0], timings[1], timings[2]])
-                print(f"Pinging {hostname}: [DONE]\n")
+            writer.writerow([hostname, timings[0], timings[1], timings[2]])
+            print(f"Pinging {hostname}: [DONE]\n")
 
 except KeyboardInterrupt:
-    print('Keyboard interrupt from a user: [OK]')
+    print("Keyboard interrupt from a user: [OK]")
