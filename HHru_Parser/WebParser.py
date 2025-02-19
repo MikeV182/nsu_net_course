9fc37469b103f317ee289a71807ec1b97110a6c3
MikeV182 <michailvays@gmail.com>
2025-02-19 23:10:34 +0700

    done parsing the website

--- HHru_Parser/WebParser.py
+++ HHru_Parser/WebParser.py
@@ -27,21 +27,21 @@ driver = webdriver.Chrome(service=service, options=options)
 
 def login():
     driver.get("https://novosibirsk.hh.ru/account/login")
-    time.sleep(5)
+    time.sleep(3)
     
     login_button = driver.find_element(
         By.XPATH,
         '/html/body/div[5]/div/div[1]/div[4]/div/div/div/div[1]/div/div/div/div \
         /div/div/div/div/div/form/div/div[3]/button[1]')
     login_button.click()
-    time.sleep(3)
+    time.sleep(2)
 
     email_label = driver.find_element(
         By.XPATH,
         '/html/body/div[5]/div/div[1]/div[4]/div/div/div/div[1]/div/div/div/div \
         /div/div/div/div/div/form/div/div/div[2]/label[2]')
     email_label.click()
-    time.sleep(3)
+    time.sleep(2)
 
     email_input = driver.find_element(By.NAME, "username")
     email_input.send_keys(LOGIN)
@@ -51,7 +51,7 @@ def login():
         '/html/body/div[5]/div/div[1]/div[4]/div/div/div/div[1]/div/div/div/div \
         /div/div/div/div/div/form/div/div/div[6]/button[2]')
     login_with_password.click()
-    time.sleep(3)
+    time.sleep(2)
     
     password_input = driver.find_element(By.NAME, "password")
     password_input.send_keys(PASSWORD)
@@ -66,40 +66,49 @@ def login():
 
 def parse_page():
     vacancies = []
-    cards = driver.find_elements(By.CLASS_NAME, "vacancy-serp-item")
+    cards = driver.find_elements(By.CLASS_NAME, "magritte-redesign")
     for card in cards:
-        try:
-            title = card.find_element(By.CLASS_NAME, "serp-item__title").text
-            company = card.find_element(By.CLASS_NAME, "vacancy-serp-item__meta-info-company").text
-            salary = card.find_element(By.CLASS_NAME, "bloko-header-section-3").text if card.find_elements(By.CLASS_NAME, "bloko-header-section-3") else "Не указана"
-            link = card.find_element(By.CLASS_NAME, "serp-item__title").get_attribute("href")
-            experience = card.find_element(By.CLASS_NAME, "vacancy-serp-item__meta-info").text
-            schedule = card.find_element(By.CLASS_NAME, "bloko-text").text
+        title = card.find_element(
+            By.CSS_SELECTOR, 
+            "span[data-qa='serp-item__title-text']").text
+        company = card.find_element(
+            By.CSS_SELECTOR, 
+            "span[data-qa='vacancy-serp__vacancy-employer-text']").text
+        salary = card.find_element(
+            By.CSS_SELECTOR, 
+            "span[class*='magritte-text_typography-label-1-regular___pi3R-_3-0-27']").text \
+            if card.find_elements(
+                By.CSS_SELECTOR, 
+                "span[class*='magritte-text_typography-label-1-regular___pi3R-_3-0-27']") \
+            else "Не указана"
+        link = card.find_element(
+            By.CSS_SELECTOR, 
+            "a[data-qa='serp-item__title']").get_attribute("href")
+        experience = card.find_element(
+            By.CSS_SELECTOR, 
+            "span[data-qa^='vacancy-serp__vacancy-work-experience']").text
             
-            vacancies.append([title, company, salary, link, experience, schedule])
-        except Exception as e:
-            print("Ошибка при парсинге карточки:", e)
+        vacancies.append([title, company, salary, link, experience])
     return vacancies
 
 def parse_vacancies():
-    url = "https://novosibirsk.hh.ru/search/vacancy?text=Python"
+    url = "https://novosibirsk.hh.ru/search/vacancy?text=PHP+junior"
     driver.get(url)
     time.sleep(3)
     
     all_vacancies = []
     page = 1
     while True:
-        print(f"Парсим страницу {page}...")
-        vacancies = parse_page()
-        all_vacancies.extend(vacancies)
-        
         try:
-            next_button = driver.find_element(By.CLASS_NAME, "bloko-button[data-qa='pager-next']")
-            next_button.click()
+            print(f"Парсим страницу {page}...")
+            vacancies = parse_page()
+            all_vacancies.extend(vacancies)
+        
+            driver.get(url + f"&page={page}")
             time.sleep(5)
             page += 1
-        except:
-            print("Достигнута последняя страница.")
+        except Exception as e:
+            print("Достигнута последняя страница или произошла ошибка:", e)
             break
     
     return all_vacancies
@@ -107,7 +116,12 @@ def parse_vacancies():
 def save_to_csv(vacancies, filename="vacancies.csv"):
     with open(filename, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(["Название вакансии", "Компания", "Зарплата", "Ссылка", "Опыт работы", "График работы"])
+        writer.writerow([
+            "Название вакансии", 
+            "Компания", 
+            "Зарплата", 
+            "Ссылка", 
+            "Опыт работы"])
         writer.writerows(vacancies)
     print(f"Данные сохранены в {filename}")
 
